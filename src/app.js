bc09c63fc080486cf29d19d9747c3351dca09b3d
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-08 19:31:10 -0300

    instalando express e rodando o server com ele

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express()
+
+const livros = [
+    {id: 1, "titulo": "senhor dos aneis"},
+    {id: 2, "titulo": "o hobbit"},
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('curso de node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+export default app
