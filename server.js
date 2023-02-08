3c71d20316abe8cb23f174e11cc1f5ce4d67ea18
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-08 18:36:49 -0300

    rotas e gitignore

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require("http")
-const port = 3000
+const port = 8888
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Página de livros',
+    '/autores': 'Página de autores',
+    '/sobre': 'Página de informações'
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type:' : 'text/plain'})
-    res.end('Curso de node')
+    res.end(rotas[req.url])
 })
 server.listen(port, () => {
     console.log(`Servidor rodando na porta http://localhost/${port}`)
