612db252c3724748bc17bfad449ded150d74584e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-09 05:18:51 -0300

    metodo post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+//transforma tudo que recebe em JSON
+app.use(express.json())
+
 const livros = [
     {id: 1, "titulo": "senhor dos aneis"},
     {id: 2, "titulo": "o hobbit"},
@@ -13,6 +16,11 @@ app.get('/', (req, res) => {
 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).json('livro cadastrado com sucesso')
 })
 
 export default app
